3a502d175a47054a84560c89fb440af2d5af5e55
Chuck Lagumbay <chuckolagumbay@gmail.com>
2021-06-13 12:37:20 +0800

    reupdate route for detainees

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 // Detainees Routes
-Route::group(['prefix' => 'detainees', 'middleware' => 'auth'], function() {
-    Route::get('subsistence', 'DetaineesController@subsistence')->name('detainees.subsistence');
-});
-
 Route::resource('detainees', 'DetaineesController')->middleware('auth');
 
 Route::get('export', 'MyController@export')->name('export');
