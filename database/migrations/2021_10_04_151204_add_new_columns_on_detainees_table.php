348cd14b0b719cd3494765735f8df9118bc5d831
Chuck Lagumbay <chuckolagumbay@gmail.com>
2021-10-07 23:19:38 +0800

    update for discharge pupc on create and edit detainee

--- database/migrations/2021_10_04_151204_add_new_columns_on_detainees_table.php
+++ database/migrations/2021_10_04_151204_add_new_columns_on_detainees_table.php
@@ -13,20 +13,9 @@ class AddNewColumnsOnDetaineesTable extends Migration
      */
     public function up()
     {
-        // Schema::create('detainees', function (Blueprint $table) {
-        //     $table->bigIncrements('id');
-        //     $table->string('first_name');
-        //     $table->string('middle_name')->nullable();
-        //     $table->string('last_name');
-        //     $table->date('birth_date')->nullable();
-        //     $table->date('detained_date')->nullable();
-        //     $table->date('released_date')->nullable();
-        //     $table->timestamps();
-        // });
-
         Schema::table('detainees', function(Blueprint $table) {
             $table->char('gender', 20)->nullable()->after('last_name');
-            $table->string('violation')->nullable()->after('gender');
+            $table->text('violation')->nullable()->after('gender');
 
             // RELEASE COLUMNS
             // blotter entry number
@@ -38,9 +27,8 @@ class AddNewColumnsOnDetaineesTable extends Migration
             // must be a foreign key to a setting for jailer list
             $table->string('released_personnel_incharge')->nullable()->after('released_blotter_number');
             $table->date('released_date_court')->nullable()->after('released_personnel_incharge');
-            $table->date('released_date_erouge')->nullable()->after('released_date_court');
-            $table->text('released_remarks')->nullable()->after('released_date_erouge');
-
+            $table->date('released_date_erogue')->nullable()->after('released_date_court');
+            $table->text('released_remarks')->nullable()->after('released_date_erogue');
         });
     }
 
@@ -54,7 +42,7 @@ class AddNewColumnsOnDetaineesTable extends Migration
         Schema::table('detainees', function (Blueprint $table) {
             $table->dropColumn(['gender', 'violation']);
 
-            $table->dropColumn(['released_blotter_number', 'released_personnel_incharge', 'released_date_court', 'released_date_erouge', 'released_remarks']);
+            $table->dropColumn(['released_blotter_number', 'released_personnel_incharge', 'released_date_court', 'released_date_erogue', 'released_remarks']);
         });
 
         // Schema::dropIfExists('detainees'); 
--- resources/views/detainees/create.blade.php
+++ resources/views/detainees/create.blade.php
@@ -18,40 +18,108 @@
                         @csrf
                         
                             <div class="form-group">
-                                <label>Name</label>
                                 <div class="row">
                                     <div class="col-md-4">
+                                        <label>First Name</label>
                                         <input type="text" class="form-control" placeholder="First Name" name="first_name" autocomplete="off">
                                     </div>
                                     <div class="col-md-4">
+                                        <label>Middle Name</label>
                                         <input type="text" class="form-control" placeholder="Middle Name" name="middle_name" autocomplete="off">
                                     </div>
                                     <div class="col-md-4">
+                                        <label>Last Name</label>
                                         <input type="text" class="form-control" placeholder="Last Name" name="last_name" autocomplete="off">
                                     </div>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <div class="row">
-                                    
-                                    <div class="col-md-4">
-                                        <label>Birth Date</label>
-                                        <input id="datepicker_birth_date" type="text" name="birth_date" autocomplete="off">
+                                    <div class="col-md-12">
+                                        <label>Law Violated</label>
+                                        <textarea class="form-control" name="violation" rows="2"></textarea>
                                     </div>
-
-                                    <div class="col-md-4">
-                                        <label>Detained Date</label>
-                                        <input id="datepicker_detained_date" type="text" name="detained_date" autocomplete="off">
-                                    </div>
-
-                                    <div class="col-md-4">
-                                        <label>Release Date</label>
-                                        <input id="datepicker_released_date" type="text" name="released_date" autocomplete="off">
-                                    </div>
-
                                 </div>
-    
                             </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label>Gender</label>
+                                            <select class="form-control">
+                                                <option value="male">Male</option>
+                                                <option value="female">Female</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <label>Birth Date</label>
+                                            <input id="datepicker_birth_date" type="text" name="birth_date" autocomplete="off">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            
+                                            <label>Detained Date</label>
+                                            <input id="datepicker_detained_date" type="text" name="detained_date" autocomplete="off">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="card">
+                                            <div class="card-header">
+                                                <strong>Discharge Detainee</strong>
+                                            </div>
+                                            <div class="card-body">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <label>Blotter Entry Number</label>
+                                                        <input type="text" class="form-control" placeholder="Blotter Entry Number" name="last_name" autocomplete="off">
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label>Personnel in charge</label>
+                                                            <select class="form-control">
+                                                                <option value="0">--- SELECT PERSONNEL ---</option>
+                                                                <option value="1">PSSg Hernane V Quintana</option>
+                                                                <option value="2">PSMs Jober Manaig</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <label>Court Release Date</label>
+                                                            <input id="datepicker_released_date_court" type="text" name="released_date_court" autocomplete="off">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <label>e-Rogue Release Date</label>
+                                                            <input id="datepicker_released_date_erogue" type="text" name="released_date_erogue" autocomplete="off">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <label>Release Date</label>
+                                                            <input id="datepicker_released_date" type="text" name="released_date" autocomplete="off">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-12">
+                                                        <label>Remarks / Reason of Release</label>
+                                                        <textarea class="form-control" name="violation" rows="2"></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         <div class="float-right">
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </div>
@@ -72,5 +140,7 @@
         $('#datepicker_birth_date').datepicker({uiLibrary: 'bootstrap4'});
         $('#datepicker_detained_date').datepicker({uiLibrary: 'bootstrap4'});
         $('#datepicker_released_date').datepicker({uiLibrary: 'bootstrap4'});
+        $('#datepicker_released_date_court').datepicker({uiLibrary: 'bootstrap4'});
+        $('#datepicker_released_date_erogue').datepicker({uiLibrary: 'bootstrap4'});
     </script>
 @endsection
